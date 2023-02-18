2205e74698513e7f65c71d1235b7b715a88b7a74
David <506234+tyoc213@users.noreply.github.com>
2023-02-17 21:57:53 -0600

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to some-pedestal
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/some_pedestal/core_test.clj
@@ -0,0 +1,7 @@
+(ns some-pedestal.core-test
+  (:require [clojure.test :refer :all]
+            [some-pedestal.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
