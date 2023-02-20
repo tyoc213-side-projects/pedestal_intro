5dd14f7525432c2e1fa2d0be6e371cbe6d1eb7ad
David <506234+tyoc213@users.noreply.github.com>
2023-02-19 21:05:04 -0600

    Setup pedestal and greet route

--- src/some_pedestal/core.clj
+++ src/some_pedestal/core.clj
@@ -1,5 +1,35 @@
 (ns some-pedestal.core
-  (:gen-class))
+  (:gen-class)
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
+
+
+
+(defn respond-hello [request]
+  {:status 200 :body "hola desde aqui... bye bye no"})
+
+
+
+
+
+
+;; (defn stop-server []
+;;   (http/stop server))
+
+
+(def routes
+  (route/expand-routes
+   #{["/greet" :get respond-hello :route-name :greet]}))
+
+(defn create-server []
+  (http/create-server
+   {::http/routes routes
+    ::http/type :jetty
+    ::http/join? true
+    ::http/port 8989}))
+
+(def server (http/start (create-server)))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
